61efb79dc1003a88265c04db4e4215b3fc7e6ba9
Manuel Amet <emmanuelamet11@gmail.com>
2022-11-29 09:03:37 +0000

    kafka producer-consumer implemented

---
+++ src/main/java/com/tlc/group/seven/orderprocessingservice/kafka/KafkaTopicConfig.java
@@ -0,0 +1,16 @@
+package com.tlc.group.seven.orderprocessingservice.kafka;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.config.TopicBuilder;
+
+public class KafkaTopicConfig {
+
+    @Bean
+    public NewTopic marketDataTopic(){
+        return TopicBuilder
+                .name("order-data")
+                .partitions(3)
+                .build();
+    }
+}
