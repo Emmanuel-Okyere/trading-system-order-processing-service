922e52e4c98bd53cc71237f299ef12585fb60a49
Manuel Amet <emmanuelamet11@gmail.com>
2022-11-30 14:40:07 +0000

    kafka implementation updated

--- src/main/java/com/tlc/group/seven/orderprocessingservice/kafka/producer/KafkaProducer.java
+++ src/main/java/com/tlc/group/seven/orderprocessingservice/kafka/producer/KafkaProducer.java
@@ -1,5 +1,6 @@
 package com.tlc.group.seven.orderprocessingservice.kafka.producer;
 
+import com.tlc.group.seven.orderprocessingservice.model.log.LogData;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -14,17 +15,28 @@ public class KafkaProducer {
     private static final Logger LOGGER = LoggerFactory.getLogger(KafkaProducer.class);
 
     private KafkaTemplate<String, String> kafkaTemplate;
+    private KafkaTemplate<String, LogData> kafkaTemplateLog;
 
-    public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
+    public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate, KafkaTemplate<String, LogData> kafkaTemplateLog) {
         this.kafkaTemplate = kafkaTemplate;
+        this.kafkaTemplateLog = kafkaTemplateLog;
     }
 
-    public void sendResponseToKafka(String data, String s){
+    public void sendResponseToKafkaMarketData(String data){
         LOGGER.info(String.format("Response sent to Kafka -> %s", data));
         Message<String> message = MessageBuilder
                 .withPayload(data)
                 .setHeader(KafkaHeaders.TOPIC, "order-data")
                 .build();
         kafkaTemplate.send(message);
+    }
+
+    public void sendResponseToKafkaLogData(LogData data){
+        LOGGER.info(String.format("LogData:: Response sent to Kafka -> %s", data.toString()));
+        Message<LogData> message = MessageBuilder
+                .withPayload(data)
+                .setHeader(KafkaHeaders.TOPIC, "log-data")
+                .build();
+        kafkaTemplateLog.send(message);
     }
 }
