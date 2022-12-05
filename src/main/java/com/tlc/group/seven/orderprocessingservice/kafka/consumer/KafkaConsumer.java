af4659c1cc8d9c3ddd58fbbd57080158d42c1518
Emmanuel Gyateng <emmanuel.gyateng@turntabl.io>
2022-12-05 08:44:43 +0000

    Uncommented the kafka

--- src/main/java/com/tlc/group/seven/orderprocessingservice/kafka/consumer/KafkaConsumer.java
+++ src/main/java/com/tlc/group/seven/orderprocessingservice/kafka/consumer/KafkaConsumer.java
@@ -1,18 +1,18 @@
-//package com.tlc.group.seven.orderprocessingservice.kafka.consumer;
-//
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.kafka.annotation.KafkaListener;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//public class KafkaConsumer {
-//
-//    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaConsumer.class);
-//    public String payload;
-//    @KafkaListener(topics = "market-data", groupId = "market-data-group")
-//    public void consumeFromKafka(String data){
-//        payload = data;
-//        LOGGER.info(String.format("Data received from Kafka -> %s", data));
-//    }
-//}
+package com.tlc.group.seven.orderprocessingservice.kafka.consumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaConsumer.class);
+    public String payload;
+    @KafkaListener(topics = "market-data", groupId = "market-data-group")
+    public void consumeFromKafka(String data){
+        payload = data;
+        LOGGER.info(String.format("Data received from Kafka -> %s", data));
+    }
+}
--- src/main/java/com/tlc/group/seven/orderprocessingservice/kafka/producer/KafkaProducer.java
+++ src/main/java/com/tlc/group/seven/orderprocessingservice/kafka/producer/KafkaProducer.java
@@ -1,42 +1,42 @@
-//package com.tlc.group.seven.orderprocessingservice.kafka.producer;
-//
-//import com.tlc.group.seven.orderprocessingservice.log.LogData;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.kafka.core.KafkaTemplate;
-//import org.springframework.kafka.support.KafkaHeaders;
-//import org.springframework.messaging.Message;
-//import org.springframework.messaging.support.MessageBuilder;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//public class KafkaProducer {
-//
-//    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaProducer.class);
-//
-//    private KafkaTemplate<String, String> kafkaTemplate;
-//    private KafkaTemplate<String, LogData> kafkaTemplateLog;
-//
-//    public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate, KafkaTemplate<String, LogData> kafkaTemplateLog) {
-//        this.kafkaTemplate = kafkaTemplate;
-//        this.kafkaTemplateLog = kafkaTemplateLog;
-//    }
-//
-//    public void sendResponseToKafkaMarketData(String data){
-//        LOGGER.info(String.format("Response sent to Kafka -> %s", data));
-//        Message<String> message = MessageBuilder
-//                .withPayload(data)
-//                .setHeader(KafkaHeaders.TOPIC, "order-data")
-//                .build();
-//        kafkaTemplate.send(message);
-//    }
-//
-//    public void sendResponseToKafkaLogData(LogData data){
-//        LOGGER.info(String.format("LogData:: Response sent to Kafka -> %s", data.toString()));
-//        Message<LogData> message = MessageBuilder
-//                .withPayload(data)
-//                .setHeader(KafkaHeaders.TOPIC, "log-data")
-//                .build();
-//        kafkaTemplateLog.send(message);
-//    }
-//}
+package com.tlc.group.seven.orderprocessingservice.kafka.producer;
+
+import com.tlc.group.seven.orderprocessingservice.log.LogData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaProducer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaProducer.class);
+
+    private KafkaTemplate<String, String> kafkaTemplate;
+    private KafkaTemplate<String, LogData> kafkaTemplateLog;
+
+    public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate, KafkaTemplate<String, LogData> kafkaTemplateLog) {
+        this.kafkaTemplate = kafkaTemplate;
+        this.kafkaTemplateLog = kafkaTemplateLog;
+    }
+
+    public void sendResponseToKafkaMarketData(String data){
+        LOGGER.info(String.format("Response sent to Kafka -> %s", data));
+        Message<String> message = MessageBuilder
+                .withPayload(data)
+                .setHeader(KafkaHeaders.TOPIC, "order-data")
+                .build();
+        kafkaTemplate.send(message);
+    }
+
+    public void sendResponseToKafkaLogData(LogData data){
+        LOGGER.info(String.format("LogData:: Response sent to Kafka -> %s", data.toString()));
+        Message<LogData> message = MessageBuilder
+                .withPayload(data)
+                .setHeader(KafkaHeaders.TOPIC, "log-data")
+                .build();
+        kafkaTemplateLog.send(message);
+    }
+}
